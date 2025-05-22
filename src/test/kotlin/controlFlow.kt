6c17d4f48672e31dc9dec78dd71d823afa31dc79
Roshankumar404 <roshankuar951@gmail.com>
2025-05-22 18:51:51 +0530

    wrote first input function in kotlin wrote control folw in kotlin variety in code

--- src/test/kotlin/controlFlow.kt
+++ src/test/kotlin/controlFlow.kt
@@ -1,10 +1,32 @@
-fun main() {
-    print("Enter the Total number")
-    val TotalNumber: Int = readln().toInt()
-    if (TotalNumber >= 80)
-        print("Good Student")
-    else if (TotalNumber >= 70)
-        print("Can do better")
-    else if (TotalNumber >= 60)
-        print("needs to do hard work")
-}
+//fun main() {
+//    print("Enter the Total number")
+//    val TotalNumber: Int = readln().toInt()
+//    if (TotalNumber >= 80)
+//        print("Good Student")
+//    else if (TotalNumber >= 70)
+//        print("Can do better")
+//    else if (TotalNumber >= 60)
+//        print("needs to do hard work")
+//}
+
+
+
+// we can use range based too
+
+fun main (){
+    print("enter the number plz")
+    val nu:Int= readln().toInt()
+
+    if(nu in 90..100){
+        println("has bright future")
+    }
+    else if( nu in 80..90){print("good future")
+     }
+    else if(nu in 70..80){
+    print("can do something better but will need very hard work")
+    }
+    else{
+        print("chhod de tu padhai")
+    }
+     }
+
