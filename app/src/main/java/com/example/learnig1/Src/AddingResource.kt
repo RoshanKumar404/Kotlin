db0f4d4ea5d51608915067a5652a3d8cb5244d7e
Roshankumar404 <roshankuar951@gmail.com>
2025-06-14 11:53:54 +0530

    learned recompositoon

--- app/src/main/java/com/example/learnig1/Src/AddingResource.kt
+++ app/src/main/java/com/example/learnig1/Src/AddingResource.kt
@@ -5,10 +5,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-@Composable
-fun Greet(name: String,age:Int,color:Color) {
-    Text(
-        text = "Hello $name ! with $age and with very ${color.toString()} welcome to the new world",
+//@Composable
+//fun Greet(name: String,age:Int,color:Color) {
+//    Text(
+//        text = "Hello $name ! with $age and with very ${color.toString()} welcome to the new world",
+//
+//    )
+//}
 
-    )
+//Recompostion learnigs
+@Composable
+fun triangle(name:String){
+    Text("jia sri ramm")
 }
