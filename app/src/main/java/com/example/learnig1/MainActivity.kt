5eee8ddfc42639542e6a5a5850aa8307e926dcaf
Roshankumar404 <roshankuar951@gmail.com>
2025-06-14 11:14:39 +0530

    todays 1st chnage how to print color hex code using parameter in function

--- app/src/main/java/com/example/learnig1/MainActivity.kt
+++ app/src/main/java/com/example/learnig1/MainActivity.kt
@@ -3,45 +3,47 @@ package com.example.learnig1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+import com.example.learnig1.Src.learnigs.Greet
 import com.example.learnig1.ui.theme.Learnig1Theme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge(false)
         setContent {
+
             Learnig1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Roshan,",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               // Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                  Greet(
+                    name="Rahul",age=23, color=Color.Gray
+                  )
+                //}
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello ${name}! welcome to the new world",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Learnig1Theme {
-        Greeting("rohan")
-    }
-}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello ${name}! welcome to the new world",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Learnig1Theme {
+//        Greeting("rohan")
+//    }
+//}
--- app/src/main/java/com/example/learnig1/Src/AddingResource.kt
+++ app/src/main/java/com/example/learnig1/Src/AddingResource.kt
@@ -1,2 +1,14 @@
-package com.example.learnig1.Src
+package com.example.learnig1.Src.learnigs
 
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Greet(name: String,age:Int,color:Color) {
+    Text(
+        text = "Hello $name ! with $age and with very ${color.toString()} welcome to the new world",
+
+    )
+}
