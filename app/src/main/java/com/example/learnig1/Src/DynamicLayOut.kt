184dbdfb65a665ef81673c90d4dbed64bd9dd85a
Roshankumar404 <roshankuar951@gmail.com>
2025-06-22 08:36:26 +0530

    some changes

--- app/src/main/java/com/example/learnig1/Src/DynamicLayOut.kt
+++ app/src/main/java/com/example/learnig1/Src/DynamicLayOut.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 //import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 //import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -15,9 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 //import androidx.compose.ui.graphics.Shape
 //import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 //@Preview()
@@ -25,26 +28,33 @@ fun Dynamic() {
     //Lazy column
     LazyRow {
         items(count = 15){index->
-            Text("jai sri ram roshan $index", modifier = Modifier
-                .padding(10.dp)
-                .height(25.dp)
+            Text("jai sri ram roshan $index", fontSize = 20.sp, modifier = Modifier
+                .padding(15.dp)
+                .height(35.dp)
                 .background(color = Color.White)
                 .border(
                     width = 4.dp,
+
                     brush =Brush.linearGradient(colors = listOf(Color.Cyan,Color.Magenta,Color.Red),
                     ),
+
                     shape=RoundedCornerShape(4.dp)
 
-                ))
+                )
+                .padding(horizontal = 20.dp, vertical = 5.dp))
+
+                //.size(15.dp))
+
         }
     }
     LazyColumn {
         items(count = 100){index->
-            Text("Roshan is very good boy $index", modifier = Modifier
+            Text("Roshan is very good boy $index", fontSize = 20.sp, fontWeight= FontWeight.Bold, modifier = Modifier
                 .padding(5.dp)
 
-                .height(25.dp)
+                .height(55.dp)
                 .background(color = Color.White)
+
                 .border(
                     width = 4.dp,
                     brush =Brush.linearGradient(colors = listOf(Color.Cyan,Color.Magenta,Color.Red),
@@ -52,6 +62,7 @@ fun Dynamic() {
                     shape=RoundedCornerShape(4.dp)
 
                 )
+                .padding(horizontal = 7.dp, vertical = 14.dp)
 //                .border(
 //                    width = 4.dp,
 //                    brush = Brush.linearGradient(colors = listOf(Color.Cyan,Color.Red,Color.Magenta),
