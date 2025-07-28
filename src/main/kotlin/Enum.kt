d5f95f388984db2e930f31d0be4fbe8066790ecf
Roshankumar404 <roshankuar951@gmail.com>
2025-07-28 08:12:08 +0530

    extending classes with new properties

--- src/main/kotlin/Enum.kt
+++ src/main/kotlin/Enum.kt
@@ -7,9 +7,24 @@ data class Question<T>(
     val answer: T,
     val difficulty: Difficulty
 )
-fun main() {
+//object StudentProgress{
+//    var total:Int=10
+//    var Asnwered: Int=3
+//}
+class LearningCompanionClass{
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question3.toString())
+    companion object StudentProgress{
+        var total:Int=10
+        var Asnwered: Int=3
+    }
+}
+val LearningCompanionClass.StudentProgress.progressText: String
+    get()="$Asnwered of $total answered"
+fun main() {
+
+    //println(question3.toString())
+    //println("${LearningCompanionClass.Asnwered} of ${LearningCompanionClass.total} answere.")
+    println(LearningCompanionClass.progressText)
 }
