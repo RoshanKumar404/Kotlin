29e8b1e70ec84856384b719f9585f1aae5be4e73
Roshankumar404 <roshankuar951@gmail.com>
2025-07-28 09:08:45 +0530

    made Quiz app functional

--- src/main/kotlin/Quiz.kt
+++ src/main/kotlin/Quiz.kt
@@ -1,43 +1,70 @@
 import java.util.Scanner
 
-class Quiz<Generictype> (
+class Quiz<Generictype>(
     val question: String,
     val answer: Generictype,
     val dificulty: String
-){
-//    override fun toString(): String {
+) {
+    //    override fun toString(): String {
 //        return "Question: $question\nAnswer: $answer\nDifficulty: $dificulty"
 //    }
+    companion object StudentProgress {
+        var total: Int = 0
+        var Asnwered: Int = 0
+        val progressText: String
+            get() = "$Asnwered/$total Answered correctly ."
+    }
 
-    fun queAndAnswer(scanner: Scanner){
+
+    fun queAndAnswer(scanner: Scanner) {
         println("Question : $question")
         print("Answer plzz : ")
-        val userInput= scanner.nextLine()
-        val isCorrect= when(answer){
-            is String->userInput.trim().equals(answer as String, ignoreCase = true)
-            is Boolean->userInput.trim().toBooleanStrictOrNull()==answer
-            is Int-> userInput.trim().toIntOrNull()==answer
+        val userInput = scanner.nextLine()
+        val isCorrect = when (answer) {
+            is String -> userInput.trim().equals(answer as String, ignoreCase = true)
+            is Boolean -> userInput.trim().toBooleanStrictOrNull() == answer
+            is Int -> userInput.trim().toIntOrNull() == answer
             else -> false
         }
         if (isCorrect) {
             println(" correct ")
+            Asnwered++
 
-        }else{
+        } else {
             println(" Incorrect, The Answer is $answer \n")
         }
+        gPrintProgressBar()
+
     }
 }
-fun main(){
+
+fun Quiz.StudentProgress.gPrintProgressBar() {
+    repeat(Asnwered) { print("▓") }
+    repeat(total - Asnwered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
+fun main() {
     val scanner = Scanner(System.`in`)
-    val instance1=Quiz<String>("Jai sri ________","Ram","Easy")
-    val instance2=Quiz<Boolean>("Roshan is the very inconsistent Boy (Boolean type)",true,"Easy")
-    val instance3=Quiz<Int>("what is the age of roshan",23,"Hard")
+    val Questions=listOf(
+        Quiz<Boolean>("Roshan is the very inconsistent Boy (Boolean type)", true, "Easy"),
+                Quiz<Int>("what is the age of roshan", 23, "Hard"),
+                Quiz<String>("Jai Sri ___", "ram", "Medium"),
+        Quiz<Int>("what is the salary of roshan", 0, "Hard")
+
+    )
+    Quiz.total=Questions.size
+
+//    val instance1 =
+//    val instance2 =
+//    val instance3 =
 
 //    println(instance3)
 //    println(instance1)
 //    println(instance2)
-    val  quizes= listOf(instance3,instance2,instance1)
-    for (quiz in quizes){
+    //val quizes = listOf(instance3, instance2, instance1)
+    for (quiz in Questions) {
         quiz.queAndAnswer(scanner)
     }
     println(" All Done ...")
